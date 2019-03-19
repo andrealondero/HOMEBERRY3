a2f0a95b899f9dd7bb26d953b7563ddaa72687a3
AlessandroViolin91 <a.violin@itsrizzoli.it>
2019-03-19 16:52:46 +0100

    bottone led 1 bis

--- app/src/main/java/com/example/homeberry3/FragmentLight1.java
+++ app/src/main/java/com/example/homeberry3/FragmentLight1.java
@@ -79,7 +79,4 @@ public class FragmentLight1 extends Fragment {
         }
     }
 
-
-
-
 }
--- app/src/main/java/com/example/homeberry3/FragmentLight2.java
+++ app/src/main/java/com/example/homeberry3/FragmentLight2.java
@@ -6,13 +6,26 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.Switch;
+
+import com.google.android.things.pio.Gpio;
+import com.google.android.things.pio.PeripheralManager;
+
+import java.io.IOException;
 
 public class FragmentLight2 extends Fragment {
 
     View view;
+    ImageButton mainterraceLight;
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private Gpio mLedGpio;
+    boolean state = false;
 
     public FragmentLight2() {
     }
